0b3c58b3748ba0691898c19416828c31aabd6e76
林士立 <t301000@gmail.com>
2018-11-25 01:56:20 +0800

    調整資料表

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -2,11 +2,12 @@ CREATE TABLE `ntpc_openid_random_pass` (
   `uname` varchar(100) NOT NULL default '',
   `random_pass` varchar(255) NOT NULL default '',
   PRIMARY KEY (`uname`)
-) ENGINE=MyISAM ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ntpc_openid_login_rules` (
+  `sn` int unsigned NOT NULL AUTO_INCREMENT,
   `rule` varchar(255) NOT NULL,
   `sort` int NOT NULL,
   `enable` tinyint(1) NOT NULL default 1,
-  PRIMARY KEY (`rule`)
-) ENGINE=MyISAM ;
+  PRIMARY KEY (`sn`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
