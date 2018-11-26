e40f60f98ebca2a7230d65b0521de38f5b00ce13
林士立 <t301000@gmail.com>
2018-11-26 22:37:16 +0800

    加入權限控管

--- admin/save_sort.php
+++ admin/save_sort.php
@@ -5,6 +5,11 @@
 
     include_once "../../../mainfile.php";
     include_once "../function.php";
+
+    if (! $xoopsUser || ! isAdmin()) {
+        die("未登入 或 不具有模組管理權");
+    }
+
     $sort = 1;
     foreach ($_POST['sn'] as $sn) {
         $sql = "update " . $xoopsDB->prefix("ntpc_openid_login_rules") . " set `sort`='{$sort}' where `sn`='{$sn}'";
@@ -12,3 +17,20 @@
         $sort++;
     }
     echo _TAD_SORTED . "(" . date("Y-m-d H:i:s") . ")";
+
+
+    /********************* function 區 *********************/
+
+    /**
+     * 是否是模組管理員
+     *
+     * @return bool
+     */
+    function isAdmin() {
+        global $xoopsUser;
+
+        $modhandler = xoops_gethandler('module');
+        $xoopsModule = $modhandler->getByDirname("ntpc_openid");
+
+        return $xoopsUser->isAdmin($xoopsModule->mid());
+    }
