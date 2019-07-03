091a2c66241aa62f9181c237535330c8cd0ad570
林士立 <t301000@gmail.com>
2019-07-03 18:09:59 +0800

    修改版號：1.1.1

--- xoops_version.php
+++ xoops_version.php
@@ -14,7 +14,7 @@ $modversion['image']       = 'images/logo.png';
 $modversion['dirname']     = basename(dirname(__FILE__));
 
 //---模組狀態資訊---//
-$modversion['release_date']        = '2019/07/02';
+$modversion['release_date']        = '2019/07/03';
 $modversion['module_website_url']  = 'https://github.com/t301000/xoops-ntpc-openid';
 $modversion['module_website_name'] = 'NTPC OpenID';
 $modversion['module_status']       = 'release';
